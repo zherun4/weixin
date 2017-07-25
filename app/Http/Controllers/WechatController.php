5ced0a3a80de26c4c064ac81e7b8e7925748eb16
zherun4 <654342486@qq.com>
2017-07-25 11:31:29 +0800

    second

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class WechatController extends Controller
+{
+
+    /**
+     * 处理微信的请求消息
+     *
+     * @return string
+     */
+    public function serve()
+    {
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            return "欢迎关注 overtrue！";
+        });
+
+
+        return $wechat->server->serve();
+    }
+}
